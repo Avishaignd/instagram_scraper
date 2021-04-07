9fde97de8ebce677d709d0df73f81e7cd5f6c3b7
Avishaignd <avishaignd@gmail.com>
2021-04-07 14:22:38 +0300

    instagram scraper working

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from scraper import Scraper
 # First we initialize the scraper with our username and password in case we need to login to instagram
 scraper = Scraper()
 
-# Then we user the get data function with the account we want to scrape
+# Then we use the get data function with the account we want to scrape
 scraper.get_data()
